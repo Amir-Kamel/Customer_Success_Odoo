5c0eceffc43f54d919dea7fd23b86667a524efc3
Amir-Kamel <amir.kghabrial@gmail.com>
2025-10-09 07:58:31 +0300

    Revert "CSM"
    This reverts commit 2fd4778df366e3b5de42f8c27ff684d8b1ac82f8.

--- customer_success/models/customer_success_lost_reason.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class CustomerSuccessLostReason(models.Model):
-    _name = 'customer.success.lost.reason'
-    _description = 'Lost Reason'
-
-    name = fields.Char(string="Reason", required=True)
